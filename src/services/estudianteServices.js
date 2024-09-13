5fd425fd1ce795eeb9587c3414b46f9051e7ce4a
nacho <nachotrotta81@gmail.com>
2024-09-13 16:04:31 -0300

    se agrego dar de baja estudiante

--- src/services/estudianteServices.js
+++ src/services/estudianteServices.js
@@ -1,19 +1,22 @@
 import axios from 'axios'
 
-const ESTUDIANTE_BASE_REST_API_URL="http://localhost:8080/api";
+const ESTUDIANTE_BASE_REST_API_URL = "http://localhost:8080/api";
 
-class EstudianteService{
-    getAllEstudiantes(){
-        return axios.get(ESTUDIANTE_BASE_REST_API_URL+"/alumnos");
+class EstudianteService {
+    getAllEstudiantes() {
+        return axios.get(ESTUDIANTE_BASE_REST_API_URL + "/alumnos");
     }
-    AltaEstudiante(Alumno){
-        return axios.post(ESTUDIANTE_BASE_REST_API_URL+"/alumnos/crear",Alumno)
+    AltaEstudiante(Alumno) {
+        return axios.post(ESTUDIANTE_BASE_REST_API_URL + "/alumno/crear", Alumno)
     }
-    getEstudianteById(id){
-        return axios.get(ESTUDIANTE_BASE_REST_API_URL+"/alumnos/"+id);
+    getEstudianteById(id) {
+        return axios.get(ESTUDIANTE_BASE_REST_API_URL + "/alumno/" + id);
     }
-    updateEstudiante(id,Alumno){
-        return axios.put(ESTUDIANTE_BASE_REST_API_URL+"/alumnos/"+id,Alumno);
+    updateEstudiante(id, Alumno) {
+        return axios.put(ESTUDIANTE_BASE_REST_API_URL + "/alumno/update/{id}" + id, Alumno);
+    }
+    deleteEstudiante(id, Alumno) {
+        return axios.put(ESTUDIANTE_BASE_REST_API_URL + "/alumno/baja/{id}" + id, Alumno);
     }
 }
 
